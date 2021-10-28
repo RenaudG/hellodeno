f116a8d6a20367fcfdc80fbb7d602d5f337dea6f
Renaud <rgombert@essentiel.net>
2021-10-28 16:21:39 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 
 function handle( event ) {
     const response = new Response(
-        "<html><body>Salut la terre - de lapart de : " + Deno.env.get('MYKEY') + "</body></html>",
+        "<html><body><h2>Salut la terre - de lapart de : " + Deno.env.get('MYKEY') + "</h2></body></html>",
         { headers: { "content-type": "text/html" },  }
     );
     event.respondWith(response);
